d65b64445a2952ce9fb5f642e467621b2d79a757
Strides-hovo <hovik_ghambaryan@mail.ru>
2026-07-24 01:57:42 +0400

    ci: update GitHub Actions workflow for tests

--- tests/Feature/Mail/MakeMailTest.php
+++ tests/Feature/Mail/MakeMailTest.php
@@ -5,45 +5,50 @@ namespace Strides\Module\Tests\Feature\Mail;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Strides\Module\Enums\BuilderKeysEnum;
+use Strides\Module\Factories\FileNameFactory;
 use Strides\Module\ModuleHelper;
 use Strides\Module\Providers\LoaderServiceProvider;
 use Strides\Module\Tests\Feature\Feature;
 
+
+/**
+ * @runInSeparateProcess
+ * @preserveGlobalState disabled
+ */
 class MakeMailTest extends Feature
 {
-    /**
-     * @runInSeparateProcess
-     *
-     * @preserveGlobalState disabled
-     */
+
+
     public function test_create_with_view(): void
     {
-        $moduleName = 'Category';
-        $fileName = 'CategoryMail';
+
+        $fileName = FileNameFactory::make($this->moduleName, BuilderKeysEnum::mail);
 
         $this->artisan('module:make-module', [
-            'moduleName' => $moduleName,
+            'moduleName' => $this->moduleName,
         ])->assertOk();
 
         $this->artisan('module:make-mail', [
-            'moduleName' => $moduleName,
+            'moduleName' => $this->moduleName,
             'fileName' => $fileName,
             '--view' => true,
+            '--force' => true
         ])->assertOk();
 
-        $file = $this->getFilePath(BuilderKeysEnum::mail, $moduleName, $fileName);
-        $class = ModuleHelper::namespace($moduleName, BuilderKeysEnum::mail, $fileName);
-        $viewPath = ModuleHelper::normalizePath(ModuleHelper::module($moduleName, 'resources/views/mail/'.Str::lower($fileName).'.blade.php'));
+        $file = $this->getFilePath(BuilderKeysEnum::mail, $this->moduleName, $fileName);
+        $class = ModuleHelper::namespace($this->moduleName, BuilderKeysEnum::mail, $fileName);
+        $viewPath = ModuleHelper::normalizePath(ModuleHelper::module($this->moduleName, 'resources/views/mail/' . Str::kebab($fileName) . '.blade.php'));
 
         (new LoaderServiceProvider($this->app))->boot();
 
-        $view = Str::lower($moduleName).'::mail.'.Str::lower($fileName);
+        $view = Str::lower($this->moduleName) . '::mail.' . Str::kebab($fileName);
 
         require $file;
 
         $this->assertTrue(File::exists($viewPath));
         $this->assertTrue(File::exists($file));
         $this->assertTrue(class_exists($class));
+
         $this->assertTrue(view()->exists($view));
     }
 }
